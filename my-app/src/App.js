3bb86dbf5099340804915e12fa5cf7c79871365d
austinbhan <austinbhan@outlook.com>
2024-11-01 10:38:55 -0700

    Basic menu and portrait placeholders without formatting

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,42 @@
 import './App.css';
+import placeholderPortrait from "./Assets/placeholderPortrait.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="header">
       <p>This is the header. Name goes here</p>
       </header>
-      <body>
+      <body className="body">
         <h3>This is the body. Menus and the cover image go here</h3>
+        <div className="menus">
+        <div className="menu-and-image">
+          <div className="menu-left">
+            <p>Musician</p>
+            <ul>
+              <li>About Me</li>
+              <li>Events</li>
+              <li>Lessons</li>
+              <li>Performances</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+          <img src={placeholderPortrait} alt="placeHolder" className="portrait"/>
+        </div>
+        <div className="menu-right">
+          <div className="menu-and-image">
+          <p>Photography</p>
+          <ul>
+            <li>Portfolio</li>
+            <li>Commissions</li>
+            <li>Contact</li>          
+          </ul>
+          <img src={placeholderPortrait} alt="placeholder" className="portrait"/>
+        </div>
+        </div>
+          </div>
       </body>
-      <footer>This is the footer. Copyright information down here</footer>
+      <footer className="footer">This is the footer. Copyright information down here</footer>
     </div>
   );
 }
